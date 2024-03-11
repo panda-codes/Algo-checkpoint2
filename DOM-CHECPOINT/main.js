1ca5be4dc513d28cdd55d3f18db412a4b0110cfd
panda-codes <dedeomatse@gmail.com>
2024-03-12 00:35:12 +0100

    created two fuctions to toggle the list of the shopping cart

--- DOM-CHECPOINT/main.js
+++ DOM-CHECPOINT/main.js
@@ -1,2 +1,16 @@
 let openShopping = document.getElementById('shoppingBag');
-let closeShopping = document.querySelector('.closeShopping')
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listcard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total')
+let quantity = document.getElementById('quantity')
+let card = document.querySelector('.card')
+
+openShopping.addEventListener('click',()=>{
+    card.classList.toggle('active');
+})
+
+closeShopping.addEventListener('click',()=>{
+    card.classList.toggle('active');
+})
